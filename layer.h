e22dd41cd109d38b78e254a36f6f3c2ff059db17
Li Zhang <lizhang@Huashan.unt.ad.unt.edu>
2018-07-05 16:58:42 -0500

    70% done

--- layer.h
+++ layer.h
@@ -58,13 +58,52 @@ __global__ void fp_preact_f2(float input[84], float preact[10], float weight[10]
 __global__ void fp_bias_f2(float preact[10], float bias[10]);
 
 // Back propagation kernels
+__global__ void bp_weight_f2(float d_weight[10][84], float d_preact[10], float p_output[84]);
+__global__ void bp_bias_f2(float bias[10], float d_preact[10]);
+
+__global__ void bp_output_f1(float d_output[84], float n_weight[10][84], float nd_preact[10]);
+__global__ void bp_preact_f1(float d_preact[84], float d_output[84], float preact[84]);
+__global__ void bp_weight_f1(float d_weight[84][120], float d_preact[84], float p_output[120]);
+__global__ void bp_bias_f1(float bias[84], float d_preact[84]);
+
+__global__ void bp_output_c3(float d_output[120], float n_weight[84][120], float nd_preact[84]);
+__global__ void bp_preact_c3(float d_preact[120], float d_output[120], float preact[120]);
+__global__ void bp_weight_c3(float d_weight[120][16][4][4], float d_preact[120], float p_output[16][4][4]);
+__global__ void bp_bias_c3(float bias[120], float d_preact[120]);
+
+__global__ void bp_output_s2(float d_output[16][4][4], float n_weight[120][16][4][4], float nd_preact[120]);
+__global__ void bp_preact_s2(float d_preact[16][4][4], float d_output[16][4][4], float preact[16][4][4]);
+__global__ void bp_weight_s2(float d_weight[1][2][2], float d_preact[16][4][4], float p_output[16][8][8]);
+__global__ void bp_bias_s2(float bias[1], float d_preact[16][4][4]);
+
+__global__ void bp_output_c2(float d_output[16][8][8], float n_weight[1][2][2], float nd_preact[16][4][4]);
+__global__ void bp_preact_c2(float d_preact[16][8][8], float d_output[16][8][8], float preact[16][8][8]);
+__global__ void bp_weight_c2(float d_weight[16][6][5][5], float d_preact[16][8][8], float p_output[6][12][12]);
+__global__ void bp_bias_c2(float bias[16], float d_preact[16][8][8]);
+
+__global__ void bp_output_s1(float d_output[6][12][12], float n_weight[16][6][5][5], float nd_preact[16][8][8]);
+__global__ void bp_preact_s1(float d_preact[6][12][12], float d_output[6][12][12], float preact[6][12][12]);
+__global__ void bp_weight_s1(float d_weight[1][2][2], float d_preact[6][12][12], float p_output[6][24][24]);
+__global__ void bp_bias_s1(float bias[1], float d_preact[6][12][12]);
+
+__global__ void bp_output_c1(float d_output[6][24][24], float n_weight[1][2][2], float nd_preact[6][12][12]);
+__global__ void bp_preact_c1(float d_preact[6][24][24], float d_output[6][24][24], float preact[6][24][24]);
+__global__ void bp_weight_c1(float d_weight[6][5][5], float d_preact[6][24][24], float p_output[28][28]);
+__global__ void bp_bias_c1(float bias[6], float d_preact[6][24][24]);
+
+
+
+
+/*
 __global__ void bp_weight_f(float d_weight[10][6][6][6], float d_preact[10], float p_output[6][6][6]);
 __global__ void bp_bias_f(float bias[10], float d_preact[10]);
 __global__ void bp_output_s1(float d_output[6][6][6], float n_weight[10][6][6][6], float nd_preact[10]);
 __global__ void bp_preact_s1(float d_preact[6][6][6], float d_output[6][6][6], float preact[6][6][6]);
 __global__ void bp_weight_s1(float d_weight[1][4][4], float d_preact[6][6][6], float p_output[6][24][24]);
 __global__ void bp_bias_s1(float bias[1], float d_preact[6][6][6]);
+
 __global__ void bp_output_c1(float d_output[6][24][24], float n_weight[1][4][4], float nd_preact[6][6][6]);
 __global__ void bp_preact_c1(float d_preact[6][24][24], float d_output[6][24][24], float preact[6][24][24]);
 __global__ void bp_weight_c1(float d_weight[6][5][5], float d_preact[6][24][24], float p_output[28][28]);
 __global__ void bp_bias_c1(float bias[6], float d_preact[6][24][24]);
+*/
